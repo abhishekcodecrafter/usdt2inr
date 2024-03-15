5b1e35f3227cc6360b3c224a9b17f18424a7fb14
Deepak <deepakd@tekion.com>
2024-03-15 08:46:21 +0530

    bug fixes

--- routes/validate_IFSC.py
+++ routes/validate_IFSC.py
@@ -1,5 +1,7 @@
 import requests
-from flask import Flask, request, jsonify 
+from flask import Flask, request, jsonify
+
+from routes.send_message import send_message
 
 app = Flask(__name__)
 
@@ -23,22 +25,26 @@ def get_bank_details(ifsc_code):
 
 @app.route('/Validate_IFSC', methods=['POST'])
 def validate_IFSC():
-    data = request.get_json()
-    ifsc_code = data.get('ifsc')
-    bank_details = get_bank_details(ifsc_code)
+    try:
+        data = request.get_json()
+        ifsc_code = data.get('ifsc')
+        bank_details = get_bank_details(ifsc_code)
 
-    if "error" in bank_details:
-        print(bank_details["error"])
-        response_data = {
-            'status': 'Failed',
-            'message': f'Invalid IFSC Code: {ifsc_code}',
-            'error': bank_details["error"]
-        }
-    else:
-        response_data = {
-            'status': 'Success',
-            'message': 'IFSC code validated successfully',
-            'bank_details': bank_details
-        }
+        if "error" in bank_details:
+            print(bank_details["error"])
+            response_data = {
+                'status': 'Failed',
+                'message': f'Invalid IFSC Code: {ifsc_code}',
+                'error': bank_details["error"]
+            }
+        else:
+            response_data = {
+                'status': 'Success',
+                'message': 'IFSC code validated successfully',
+                'bank_details': bank_details
+            }
 
-    return jsonify(response_data)
+        return jsonify(response_data)
+    except Exception as e:
+        send_message(f"Error in fetching ifsc code : {e}")
+        raise e
